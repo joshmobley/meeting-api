e64434ff74e7dcb1764d34eee17359eb4e312565
Josh Mobley <joshua.mobley@consensys.net>
2019-02-14 00:16:10 -0500

    cleanup db.js

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
-//const mongoose = require('mongoose')
+
 const dotenv = require('dotenv')
-dotenv.load();
 const Sequelize = require('sequelize')
+
+dotenv.load();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_ENDPOINT,
